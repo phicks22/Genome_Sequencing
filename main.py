9b7e2091c36bb76da56385be39bfcea38d9eb74e
phicks22 <65268541+phicks22@users.noreply.github.com>
2022-06-27 15:21:27 -0600

    Completed divide_sequence function to divide a genome sequence into kmer fragments with overlaps.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # Algorithms for Coursera course "Genome Sequencing: Bioinformatics II."
+import numpy as np
 
-def divide(sequence, k):
+
+def divide_sequence(sequence, k):
     """
     Divides a genome sequence into n k-mer fragments to be sorted in lexicographic order.
 
     :param sequence: Genome sequence input as a string.
     :param k: K-mer length (nucleotide length of each genome fragment).
-    :return: Divided genome fragment within an array.
+    :return: Divided kmer fragments as a space delimited .txt file.
     """
 
-    div_string = [sequence[i:i+k] for i in range(0, len(sequence), k)]
+    div_string = np.array([sequence[i:i + k] for i in range(0, len(sequence) - k + 1)])
 
-    return div_string
+    with open("output.txt", 'w') as filehandle:
+        filehandle.writelines("%s " % kmer for kmer in div_string)
 
+    return filehandle
+
+
+# Example output
+sequence = open("Data/dataset_197_3.txt", "r").read()
+divide_sequence(sequence, 100)
