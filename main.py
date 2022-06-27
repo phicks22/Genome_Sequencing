ec9126eaea4b90d20cca4ecfcbf8925d3f1d1ea4
phicks22 <65268541+phicks22@users.noreply.github.com>
2022-06-27 14:10:46 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Algorithms for Coursera course "Genome Sequencing"
+
+def divide(sequence, k):
+    """
+    Divides a genome sequence into n k-mer fragments to be sorted in lexicographic order.
+
+    :param sequence: Genome sequence input as a string.
+    :param k: K-mer length (nucleotide length of each genome fragment).
+    :return: Divided genome fragment within an array.
+    """
+
+    div_string = [sequence[i:i+k] for i in range(0, len(sequence), k)]
+
+    return div_string
+
